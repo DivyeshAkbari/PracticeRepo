1994a021673823372b31bcf5e763669ed32c92e1
NikharPanchal <Panchalnikhar4920@gmail.com>
2024-06-14 22:11:31 +0530

    correct message

--- src/Signin.java
+++ src/Signin.java
@@ -1,6 +1,6 @@
 public class Signin {
 
     public static void main(String[] args) {
-        System.out.println("Sigin in functionality by divyesh");
+        System.out.println("Sigin in functionality by nikhar");
     }
 }
